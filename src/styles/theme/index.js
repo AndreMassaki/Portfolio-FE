95d4ccf030bcfc0bb3217e5c9516d81bfcb8e735
AndreMassaki <andremassaki2004@gmail.com>
2023-01-22 12:50:01 -0300

    initial changes

---
+++ src/styles/theme/index.js
@@ -0,0 +1,26 @@
+const theme = {
+  default: {
+    green: '#1EF956',
+    purple: '#BD93F9',
+    pink: '#FF79C6',
+    background: {
+      light: '#44475A',
+      dark: '#282A36',
+    },
+    remaining: {
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+  },
+  breakpoints: {
+    xxs: '320',
+    xs: '375',
+    sm: '425',
+    md: '768',
+    lg: '1024',
+    xl: '1200',
+    xxl: '1440',
+  },
+};
+
+export default theme;
